88cbaef152c44c60bf2430feb1b0a5166041456a
Mpompeu41 <89050561+Mpompeu41@users.noreply.github.com>
2022-01-20 07:48:05 -0300

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
+
+
+@Module({
+  imports: [UserModule, ProductsModule, AuthModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
